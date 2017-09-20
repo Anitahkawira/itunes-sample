433df93d81c0521b60a4c44f788d5e7c99eb64dc
Anitahkawira <annitahkawira@gmail.com>
2017-09-20 12:05:07 +0300

    initial work

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ let player = new AudioPlayer();
 
 function renderItems(data) {
     // render items to the browser
+    const ul = document.querySelector('ul.playlist');
+
+    ul.innerHTML ='';
+
+    data.forEach(function(item){
+    	const li = document.createElement('li');
+    	li.textContent = item.trackName;
+
+    	li.onclick = (e) =>{
+    		player.play(item);    		
+    	}
+
+    	ul.appendChild(li);
+    	// console.log(item);
+    })
 }
 
 
@@ -9,10 +24,27 @@ const searchForm = document.querySelector('form');
 
 searchForm.onsubmit = (e) => {
 
+	console.log('My form was submitted');
+
     // prevent normal form submission
     e.preventDefault();
 
     // fetch items here
 
-    let endpoint = 'https://itunes.apple.com/search?term=bob+dylan';
+    let artistName = document.querySelector('#artist').value;
+
+    let endpoint = 'https://itunes.apple.com/search?term=' + artistName;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', (e) =>{
+    	// console.log(xhr.responseText);
+    	let response = JSON.parse(xhr.responseText);
+    	renderItems(response.results);
+    	// console.log(response);
+    });
+
+    xhr.open('GET', endpoint, true);
+
+    xhr.send();
 }
